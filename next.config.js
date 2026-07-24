c20dea09b8bd176359f70bcff51a51660864793b
suanlatudou <102940802+suanlatudou@users.noreply.github.com>
2026-07-24 21:06:38 +0800

    Configure Cloudflare Pages static export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
+
   transpilePackages: ['lunar-javascript'],
 };
 
